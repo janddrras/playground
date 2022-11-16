e9130d833021dff28e71725c9ae99034a15be576
jandras <git+janddrras@users.noreply.github.com>
2022-11-16 09:34:51 +0200

    tailwind css setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/layouts/Layout'
-import './globals.css'
+// import './globals.css'
+import './tw-globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
